0ec86fc9fc76a4860b32a5f5f14d9bea450bb32b
Henri Bergius <henri.bergius@iki.fi>
2008-08-24 15:25:14 +0300

    Move the code-xx element checks to a separate MidgardRootFile in case somebody still needs them

--- midcom_core/httpd/midcom-root-midgard.php
+++ midcom_core/httpd/midcom-root-midgard.php
@@ -7,43 +7,17 @@
  *
  * MidgardRootFile for running MidCOM 3 under Apache
  */
-// If code-compat is defined we use that, otherwise we load MidCOM 3
-if (mgd_is_element_loaded('code-compat'))
-{
-    ?><(code-compat)><?php
-}
-else
-{
-    // Note: your MidCOM base directory has to be in PHP include_path
-    require('midcom_core/framework.php');
-    $_MIDCOM = new midcom_core_midcom('midgard');
-}
+// Load MidCOM 3
+// Note: your MidCOM base directory has to be in PHP include_path
+require('midcom_core/framework.php');
+$_MIDCOM = new midcom_core_midcom('midgard');
 
-// code- elements used for things run before output
+// Call the controller if available
+$_MIDCOM->process();
+
+$_MIDCOM->templating->template();
+
+// Read contents from the output buffer and pass to MidCOM rendering
+$_MIDCOM->templating->display();
+unset($_MIDCOM);
 ?>
-<(code-global)>
-<?php
-if (mgd_is_element_loaded('code-init'))
-{
-    ?><(code-init)><?php
-}
-elseif (isset($_MIDCOM))
-{
-    // Call the controller if available
-    $_MIDCOM->process();
-}
-
-if (isset($_MIDCOM))
-{
-    $_MIDCOM->templating->template();
-
-    // Read contents from the output buffer and pass to MidCOM rendering
-    $_MIDCOM->templating->display();
-}
-else
-{
-    ?><(ROOT)><?php
-}
-// code-finish can be used for custom caching etc
-?>
-<(code-finish)>
